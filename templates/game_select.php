6ed9e583bc7231726da9db4621262d5ef62ee54f
bawa1233333 <162896196+bawa1233333@users.noreply.github.com>
2024-03-13 11:38:14 +0530

    Update game_select.php

--- templates/game_select.php
+++ templates/game_select.php
@@ -1,1 +1,18 @@
+<?php 
+// Initialize variables
+$first_letter = '';
+$full_name = '';
+
+if(isset($_GET['letters'])) {
+	$full_name = sanitize_text_field($_GET['letters']);
+	$first_letter = strtoupper(substr($full_name, 0, 1));
+}
+?>
+
+<form method="GET">
+	<input type="hidden" name="first_letter" value="<?php echo $first_letter; ?>">
+	<input type="hidden" name="full_name" value="<?php echo $full_name; ?>">
+	<a href="../game_one_letter?letter=<?php echo $first_letter; ?>">One Letter</a>
+	<a href="../game_all_letters?letters=<?php echo $full_name; ?>">All Letters</a>
+</form>
 
